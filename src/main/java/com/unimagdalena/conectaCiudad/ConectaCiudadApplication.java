033a7e59bfef2386763d02afb90e9a17acdaa256
pbarrera15 <pbarrera@unimagdalena.edu.co>
2025-10-19 17:44:11 -0500

    plantilla del proyecto

---
+++ src/main/java/com/unimagdalena/conectaCiudad/ConectaCiudadApplication.java
@@ -0,0 +1,13 @@
+package com.unimagdalena.conectaCiudad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaCiudadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaCiudadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unimagdalena/conectaCiudad/ConectaCiudadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimagdalena.conectaCiudad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaCiudadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
