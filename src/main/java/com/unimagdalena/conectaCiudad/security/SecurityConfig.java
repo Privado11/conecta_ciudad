931d54925a42d49bcf1dd856b8d8bb3d5266dd46
pbarrera15 <pbarrera@unimagdalena.edu.co>
2025-10-30 17:44:21 -0500

    filtro jwtValidation anadido y terminado

--- src/main/java/com/unimagdalena/conectaCiudad/security/SecurityConfig.java
+++ src/main/java/com/unimagdalena/conectaCiudad/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.unimagdalena.conectaCiudad.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import com.unimagdalena.conectaCiudad.repositories.AccesoRepository;
 import com.unimagdalena.conectaCiudad.repositories.UsuarioRepository;
 import com.unimagdalena.conectaCiudad.security.filters.JwtAuthenticationFilter;
+import com.unimagdalena.conectaCiudad.security.filters.JwtValidationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -35,13 +37,16 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http, UsuarioRepository usuarioRepository, AccesoRepository accesoRepository) throws Exception {
         return http
-        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-
-        .addFilter(new JwtAuthenticationFilter(authenticationManager(), usuarioRepository, accesoRepository))
 
         .csrf(csrf -> csrf.disable())
 
         .cors(cors-> cors.disable())
+
+        .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.GET, "/usuarios/*").permitAll().anyRequest().authenticated())
+
+        .addFilter(new JwtAuthenticationFilter(authenticationManager(), usuarioRepository, accesoRepository))
+
+        .addFilterBefore(new JwtValidationFilter(authenticationManager()), JwtAuthenticationFilter.class)
 
         .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
