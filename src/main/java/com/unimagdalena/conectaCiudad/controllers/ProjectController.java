0889cbae5d01ec4077a95fadf23e3e419bd5e98e
Privado11 <wjimenez@unimagdalena.com>
2025-10-30 20:30:13 -0500

    Se agrega BD Neon

--- src/main/java/com/unimagdalena/conectaCiudad/controllers/ProjectController.java
+++ src/main/java/com/unimagdalena/conectaCiudad/controllers/ProjectController.java
@@ -21,7 +21,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
+
 
 import java.util.List;
 import jakarta.validation.Valid;
