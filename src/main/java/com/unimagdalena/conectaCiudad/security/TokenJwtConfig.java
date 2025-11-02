d3210ed0d25d456d31c9784ae4ddcc2f0d121cfd
Privado11 <wjimenez@unimagdalena.com>
2025-11-02 11:58:20 -0500

    Se modifica la secret key de dinámica a estática

--- src/main/java/com/unimagdalena/conectaCiudad/security/TokenJwtConfig.java
+++ src/main/java/com/unimagdalena/conectaCiudad/security/TokenJwtConfig.java
@@ -1,12 +1,17 @@
 package com.unimagdalena.conectaCiudad.security;
 
 import javax.crypto.SecretKey;
-
+import javax.crypto.spec.SecretKeySpec;
 import io.jsonwebtoken.Jwts;
 
 public class TokenJwtConfig {
-    public static final String PREFIX_TOKEN="Bearer ";
-    public static final String HEADER_AUTHORIZATION="Authorization";
-    public static final String CONTENT_TYPE="application/json";
-    public static final SecretKey SECRET_KEY = Jwts.SIG.HS256.key().build();
+    public static final String PREFIX_TOKEN = "Bearer ";
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String CONTENT_TYPE = "application/json";
+
+
+    private static final String SECRET_STRING = "s822fe35bc9b90aaef9a631374898e3e1";
+
+    public static final SecretKey SECRET_KEY =
+            new SecretKeySpec(SECRET_STRING.getBytes(), Jwts.SIG.HS256.key().build().getAlgorithm());
 }
